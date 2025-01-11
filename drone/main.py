862fa060722875f3fd9061c4ad6caae405d4372c
DBinK <DBinKv1@Gmail.com>
2025-01-11 18:22:22 +0800

    refactor(drone): 重构无人机控制逻辑
    - 移除 LED 初始化到 now_recv 模块
    - 调整电机初始化顺序
    - 优化数据处理和摇杆映射逻辑
    - 添加摇杆死区和映射系数配置
    - 重构 read_espnow 函数，增加数据校准和活动状态检测
    - 调整 LED 闪烁逻辑，根据摇杆和按键状态控制
    - 优化主循环逻辑，提高代码可读性和效率

--- drone/main.py
+++ drone/main.py
@@ -14,65 +14,35 @@ time.sleep(1)  # 防止点停止按钮后马上再启动导致 Thonny 连接不�
 # motor_4 = MotorESC(33)
 
 motor_1 = MotorESC(18)
-motor_2 = MotorESC(16)
+motor_2 = MotorESC(16) 
 motor_3 = MotorESC(21)
 motor_4 = MotorESC(17)
 
-# 初始化 LED
-led = Pin(15, Pin.OUT, value=1)
-
-DEAD_AREA = 20  # 摇杆死区
-MAP_COEFF = 58  # 摇杆映射系数 (根据实际需求调整)
 
 while True:
 
-    time.sleep(0.001)
-
-    data = read_espnow()
+    data , stick_work = read_espnow()
 
     if data:
-
-        lx = data[1]  
-        ly = data[2]
-        rx = data[3]
-        ry = data[4]
-        other = data[6]
-
-        if other != 0:
+        
+        if data[6] != 0x0:
             motor_1.reset()
             motor_2.reset()
             motor_3.reset()
             motor_4.reset()
-            
-            led.value(not led.value())  # 闪烁led
-            continue
-        
-
-        # print(f"原始数据: lx={lx}, ly={ly}, rx={rx}, ry={ry}")
-
-        lx += 16
-        ly += 35
-        rx += 6
-        ry += 16
-
-        print(f"矫正后数据: lx={lx}, ly={ly}, rx={rx}, ry={ry}")
-
-        # 检查lx, ly, rx, ry中是否至少有一个绝对值超过设定值
-        stick_work = (
-               abs(lx-127) > DEAD_AREA
-            or abs(ly-127) > DEAD_AREA
-            or abs(rx-127) > DEAD_AREA
-            or abs(ry-127) > DEAD_AREA
-        )
 
         if stick_work:
-            led.value(not led.value())  # 闪烁led
 
+            ly = data[1]
+            lx = data[2]
+            ry = data[4]
+            rx = data[3]
+            
             # 底盘控制
-            _ly = map_value(ly, (0, 255), (-127, 127))  if abs(ly-127) > DEAD_AREA else 0
-            _lx = map_value(lx, (0, 255), (-127, 127))  if abs(lx-127) > DEAD_AREA else 0
-            _ry = map_value(ry, (0, 255), (-127, 127))  if abs(ry-127) > DEAD_AREA else 0
-            _rx = map_value(rx, (0, 255), (-127, 127))  if abs(rx-127) > DEAD_AREA else 0
+            _ly = map_value(ly, (0, 255), (-127, 127))  
+            _lx = map_value(lx, (0, 255), (-127, 127))  
+            _ry = map_value(ry, (0, 255), (-127, 127))  
+            _rx = map_value(rx, (0, 255), (-127, 127))
 
             print(f"摇杆映射后数据: lx={_lx}, ly={_ly}, rx={_rx}, ry={_ry}")
 
@@ -84,9 +54,11 @@ while True:
             print(f"摇杆缩放后数据: lx={_lx}, ly={_ly}, rx={_rx}, ry={_ry}")
 
             motor_1.set_thr_relative(_ly)
-            motor_3.set_thr_relative(_lx)
-            motor_2.set_thr_relative(_ry)
+            motor_2.set_thr_relative(_lx)
+            motor_3.set_thr_relative(_ry)
             motor_4.set_thr_relative(_rx)
 
-        else:
-            led.value(0)
+
+    # time.sleep(0.1)
+    time.sleep(0.001)
+
--- drone/modules/now_recv.py
+++ drone/modules/now_recv.py
@@ -1,10 +1,14 @@
 
+import re
 import time
 import json
 import struct
 
 import espnow
 import network
+from machine import Pin
+
+from modules.utils import map_value
 
 
 # 初始化 WiFi 和 espnow
@@ -17,6 +21,18 @@ now.active(True)  # 连接dk广播地址
 now.add_peer(b"\xff\xff\xff\xff\xff\xff")
 
 
+# 初始化 LED
+led = Pin(15, Pin.OUT, value=1)
+
+
+DEAD_AREA = 20  # 摇杆死区
+MAP_COEFF = 58  # 摇杆映射系数 (根据实际需求调整)
+
+OFFSET_lx = 16  # 摇杆校准偏移值
+OFFSET_ly = 35
+OFFSET_rx = 6
+OFFSET_ry = 16
+
 
 def read_espnow():
     """读取espnow数据并进行解包处理"""
@@ -25,13 +41,29 @@ def read_espnow():
     #print("espnow数据:", msg)
 
     if msg:  # 如果没有数据，则返回
-        # 假设 msg 是字节串，需要先解码为字符串
-        msg_str = msg.decode('utf-8')
-        data = json.loads(msg_str)  # 处理接收到的数据
-        return data
+        
+        msg_str = msg.decode('utf-8')  # msg 解码为字符串
+        data = json.loads(msg_str)     # 处理接收到的数据
 
-    else:
-        return None
+        data[1] += OFFSET_lx   # lx 摇杆校准
+        data[2] += OFFSET_ly   # ly 摇杆校准
+        data[3] += OFFSET_rx   # rx 摇杆校准
+        data[4] += OFFSET_ry   # ry 摇杆校准
+
+        print(f"矫正后数据: lx={data[1]}, ly={data[2]}, rx={data[3]}, ry={data[4]}")
+
+        # 检查任意摇杆是否在活动状态
+        stick_work = any(abs(value - 127) > DEAD_AREA for value in data[1:5])  
+
+        if stick_work or data[5] != 0x8 or data[6] != 0x0:  # 如果任意摇杆或按键在活动状态，则闪烁led
+            led.value(not led.value())  # 闪烁led
+        else:  
+            led.value(0)   # 如果所有摇杆和按键都不在活动状态，则关闭led
+
+        return data, stick_work
+    
+    else:  # 如果没有数据，则返回
+        return None, False
 
 
 if __name__ == "__main__":
@@ -55,7 +87,7 @@ if __name__ == "__main__":
         "rx": 0,  # 右摇杆X轴 0x00 ~ 0xFF (0~255)
         "ry": 0,  # 右摇杆Y轴 0x00 ~ 0xFF (0~255)
 
-        "abxy & dpad": 0,
+        "abxy & dpad": 0,  
         "ls & rs & start & back": 0,
 
         "mode": 0x06,  # 预留模式位
